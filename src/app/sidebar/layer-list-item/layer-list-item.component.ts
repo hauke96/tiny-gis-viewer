f5218aecabe3e14d14ed74e2dc90c7f3f9f8754b
Hauke Stieler <mail@hauke-stieler.de>
2025-03-30 15:51:05 +0200

    Extent use of i18n

--- src/app/sidebar/layer-list-item/layer-list-item.component.ts
+++ src/app/sidebar/layer-list-item/layer-list-item.component.ts
@@ -1,16 +1,26 @@
 import {Component, Input} from '@angular/core';
 import {Layer} from '../../layer/layer';
+import {TranslatePipe, TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-layer-list-item',
-  imports: [],
+  imports: [
+    TranslatePipe
+  ],
   templateUrl: './layer-list-item.component.html',
   styleUrl: './layer-list-item.component.scss'
 })
 export class LayerListItemComponent {
   @Input() layer!: Layer;
 
-  onCheckboxClicked(event: Event) {
+  constructor(private translate: TranslateService) {
+  }
+
+  public onCheckboxClicked(event: Event) {
     this.layer.setVisible((event.target as HTMLInputElement).checked);
+  }
+
+  public get tooltipText(): string {
+    return this.translate.instant('wms-layer-tooltip', {name: this.layer.name});
   }
 }
--- src/app/sidebar/layer-list/layer-list.component.ts
+++ src/app/sidebar/layer-list/layer-list.component.ts
@@ -2,13 +2,15 @@ import {Component} from '@angular/core';
 import {LayerService} from '../../layer/layer.service';
 import {LayerListItemComponent} from '../layer-list-item/layer-list-item.component';
 import {AsyncPipe, NgForOf} from '@angular/common';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-layer-list',
   imports: [
     LayerListItemComponent,
     NgForOf,
-    AsyncPipe
+    AsyncPipe,
+    TranslatePipe
   ],
   templateUrl: './layer-list.component.html',
   styleUrl: './layer-list.component.scss'
