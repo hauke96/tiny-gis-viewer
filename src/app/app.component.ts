26891985d8c8d0789dd5f55b4f21758d2a21aa4e
Hauke Stieler <mail@hauke-stieler.de>
2025-03-30 15:34:29 +0200

    Add ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {MapComponent} from './map/map.component';
 import {LayerService} from './layer/layer.service';
 import {SidebarComponent} from './sidebar/sidebar/sidebar.component';
+import {TranslateService} from '@ngx-translate/core';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +12,33 @@ import {SidebarComponent} from './sidebar/sidebar/sidebar.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  title = 'tiny-gis-viewer';
+  private readonly supportedLanguages = ['de', 'en'];
+  private readonly defaultLanguage = 'en';
 
-  constructor(private layerService: LayerService) {
+  constructor(
+    private layerService: LayerService,
+    private translate: TranslateService,
+    title: Title
+  ) {
+    this.translate.addLangs(this.supportedLanguages);
+    this.translate.setDefaultLang(this.defaultLanguage);
+
+    const storedLanguage = localStorage.getItem("lang");
+    const browserLang = translate.getBrowserLang();
+
+    if (!!storedLanguage && storedLanguage !== "") {
+      translate.use(storedLanguage);
+    } else if (browserLang && this.supportedLanguages.some(l => browserLang?.match(l))) {
+      translate.use(browserLang);
+    } else {
+      translate.use(this.defaultLanguage);
+    }
+
+    translate.onLangChange.subscribe(() => {
+      translate.get('title').subscribe((res: string) => {
+        title.setTitle(res);
+      });
+    });
   }
 
   ngOnInit() {
