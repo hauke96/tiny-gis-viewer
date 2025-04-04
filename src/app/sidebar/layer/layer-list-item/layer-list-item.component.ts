6cc72c5ea1506a4eb2b119d840a58b443f0911f3
Hauke Stieler <mail@hauke-stieler.de>
2025-04-04 13:32:23 +0200

    Re-add list item component

---
+++ src/app/sidebar/layer/layer-list-item/layer-list-item.component.ts
@@ -0,0 +1,29 @@
+import {Component, Input} from '@angular/core';
+import {Layer} from '../../../layer/layer';
+import {TranslateService} from '@ngx-translate/core';
+import {CheckboxComponent} from '../../../common/checkbox/checkbox.component';
+
+@Component({
+  selector: 'app-layer-list-item',
+  imports: [
+    CheckboxComponent
+  ],
+  templateUrl: './layer-list-item.component.html',
+  styleUrl: './layer-list-item.component.scss'
+})
+export class LayerListItemComponent {
+  @Input()
+  layer!: Layer;
+
+  constructor(private translate: TranslateService) {
+  }
+
+  getTooltipText(layer: Layer): string {
+    // TODO rename translation key?
+    return this.translate.instant("wms-layer-tooltip", {name: layer.title});
+  }
+
+  onLayerSelectionClicked(layer: Layer, layerVisible: boolean) {
+    layer.setVisible(layerVisible);
+  }
+}
--- src/app/sidebar/layer/layer-list/layer-list.component.ts
+++ src/app/sidebar/layer/layer-list/layer-list.component.ts
@@ -1,18 +1,19 @@
 import {Component, Input} from '@angular/core';
 import {LayerService} from '../../../layer/layer.service';
-import {CheckboxComponent} from '../../../common/checkbox/checkbox.component';
 import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
-import {TranslatePipe, TranslateService} from '@ngx-translate/core';
+import {TranslatePipe} from '@ngx-translate/core';
 import {Layer} from '../../../layer/layer';
+import {Observable} from 'rxjs';
+import {LayerListItemComponent} from '../layer-list-item/layer-list-item.component';
 
 @Component({
   selector: 'app-layer-list',
   imports: [
-    CheckboxComponent,
     NgForOf,
     AsyncPipe,
     TranslatePipe,
-    NgIf
+    NgIf,
+    LayerListItemComponent
   ],
   templateUrl: './layer-list.component.html',
   styleUrl: './layer-list.component.scss'
@@ -20,14 +21,10 @@ import {Layer} from '../../../layer/layer';
 export class LayerListComponent {
   @Input() expanded!: boolean;
 
-  constructor(protected layerService: LayerService, private translate: TranslateService) {
+  constructor(protected layerService: LayerService) {
   }
 
-  getTooltipText(layer: Layer): string {
-    return this.translate.instant("wms-layer-tooltip", {name: layer.title});
-  }
-
-  onLayerSelectionClicked(layer: Layer, layerVisible: boolean) {
-    layer.setVisible(layerVisible);
+  public get layers(): Observable<Layer[]> {
+    return this.layerService.layers;
   }
 }
