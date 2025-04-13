45d791df0a61a20a936bc3b3cb12bdf5e9daaae1
Hauke Stieler <mail@hauke-stieler.de>
2025-04-13 11:21:24 +0200

    Show layer list in legend graphic view

--- src/app/sidebar/legend-graphic/legend-graphic-view/legend-graphic-view.component.ts
+++ src/app/sidebar/legend-graphic/legend-graphic-view/legend-graphic-view.component.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {IconButtonComponent} from '../../../common/icon-button/icon-button.component';
+import {Layer, WmsCapabilitiesLayer, WmsLayer} from '../../../map/layer';
+import {LayerService} from '../../../map/layer.service';
+import {map, Observable} from 'rxjs';
+import {AsyncPipe, NgForOf} from '@angular/common';
+import {LucideAngularModule} from 'lucide-angular';
 
 @Component({
   selector: 'app-legend-graphic-view',
   imports: [
-    IconButtonComponent
+    IconButtonComponent,
+    NgForOf,
+    AsyncPipe,
+    LucideAngularModule
   ],
   templateUrl: './legend-graphic-view.component.html',
   styleUrl: './legend-graphic-view.component.scss'
 })
 export class LegendGraphicViewComponent {
   protected expanded = true;
+
+  constructor(protected layerService: LayerService) {
+  }
+
+  public get layers(): Observable<Layer[]> {
+    return this.layerService.layers
+      .pipe(
+        map((layers: Layer[]) => {
+          const unwrappedLayers: Layer[] = [];
+
+          layers.forEach(layer => {
+            if (layer instanceof WmsCapabilitiesLayer) {
+              unwrappedLayers.push(...layer.wmsLayers);
+            } else if (layer instanceof WmsLayer) {
+              unwrappedLayers.push(layer);
+            }
+          })
+
+          return unwrappedLayers;
+        }),
+      );
+  }
 }
